50664e1e1cc452d0b90c7c1bfea0cc6cf88e2b58
Devi Dwi Novitasari <113223746+devidwinovitasari@users.noreply.github.com>
2023-06-23 14:53:51 +0700

    Add files via upload

---
+++ OPREC2023220441100090.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oprec.pkg2023.pkg220441100090;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lenovo
+ */
+
+public class OPREC2023220441100090 {
+    Scanner input = new Scanner (System.in);
+    String biasa, dobel, special;
+    String GEPREK17;
+    
+    void harga(){
+        int biasa = 5000;
+        int dobel = 7000;
+        int special = 9000;
+        
+    }
+    
+    void diskon(){
+        int diskon = 3;
+    }
+    
+//    if== 1
+//    while{
+//            System.out.println("");
+//    }
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("============Selamat Datang Di Ayam Geprek Kamal============");
+        System.out.println("=====================Menu======================");
+        System.out.println("1. Pesan Menu");
+        System.out.println("2. Lihat Pesan");
+        System.out.println("3. Selesaikan Pemesanan");
+        System.out.println("===============================================");
+        System.out.println("Pilih Menu (1-3)");
+        System.out.println("============Paket Menu Yang Tersedia============");
+        System.out.println("1. Biasa Rp.5000.0");
+        System.out.println("2. Double Rp.7000.0");
+        System.out.println("3. Special Rp.9000.0");
+        System.out.println("================================================");
+        System.out.println("Apakah tambah nasi (ya/tidak)");
+        System.out.println("Apakah ingin memesan lagi (ya/tidak)");
+    }
+    
+}
